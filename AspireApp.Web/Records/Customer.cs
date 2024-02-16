98bb6a0bf0049cef217c6a4b2c2b6f880769e094
Wilder Medeiros <wilder.sistemas@lar.ind.br>
2024-02-16 18:04:50 -0300

    Improving the create customer form

--- AspireApp.Web/Records/Customer.cs
+++ AspireApp.Web/Records/Customer.cs
@@ -1,9 +1,22 @@
-﻿namespace AspireApp.Web.Records
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspireApp.Web.Records
 {
-    public record Customer
+    public class Customer
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
+        public string? LastName { get; set; }
+        [Required]
         public string? Email { get; set; }
+        [Required]
+        public string? PhoneNumber { get; set; }
+        public DateOnly DateOfBirth { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        [Required]
+        public string? Adress { get; set; }
+        public DateOnly ClientSince { get; set; }
+        [Required]
+        public float Balance { get; set; }
     }
 }
